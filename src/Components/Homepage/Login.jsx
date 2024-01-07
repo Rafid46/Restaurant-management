e1c7b71bd63ab446b2924d264720cd943c654c57
Rafid46 <mostafarafid45@gmail.com>
2024-01-07 16:16:48 +0600

    login redesigned

--- src/Components/Homepage/Login.jsx
+++ src/Components/Homepage/Login.jsx
@@ -48,35 +48,35 @@ const Login = () => {
       });
   };
   return (
-    <div>
+    <div className="">
       <Helmet>
         <title>Delights || Login</title>
       </Helmet>
       <div
         //   style={{ backgroundImage: `url(${bannerImage})` }}
-        className="bg-cover bg-center h-[1000px] max-w-10xl mx-auto bg-gray-950"
+        className="bg-cover bg-center h-[1000px] max-w-10xl mx-auto bg-gray-950 border-4  border-white rounded-lg"
       >
         <NavBar></NavBar>
         <div className="mt-20">
-          <div className="flex items-center justify-center">
-            <div className="card rounded-lg w-72">
+          <div className="flex items-center justify-center ">
+            <div className="p-10 border-opacity-30 bg-opacity-5 bg-white shadow-sm backdrop-filter backdrop-blur-10 rounded-lg w-80 border-2  border-white">
               <h1 className="text-center font-bold text-5xl text-white">
                 Login
               </h1>
               <form onSubmit={handleLogin} className="my-6">
                 <input
-                  className="p-2 px-4 my-2 w-full rounded-full focus:outline-blue-600"
+                  className="p-2 px-4 my-2 w-full bg-transparent border-2 border-opacity-10 text-gray-200 rounded-lg focus:outline-blue-600"
                   placeholder="Email"
                   type="email"
                   name="email"
                 ></input>
                 <input
-                  className="p-2 px-4 my-2 w-full  rounded-full focus:outline-blue-600"
+                  className="p-2 px-4 my-2 w-full bg-transparent border-2 border-opacity-10 text-gray-200 rounded-lg focus:outline-blue-600"
                   placeholder="Password"
                   type="password"
                   name="password"
                 ></input>
-                <button className="w-full bg-orange-500 h-[40px] my-3 rounded-full cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0 text-[#ffffff] font-medium">
+                <button className="rounded-lg w-full bg-orange-500 h-[40px] my-3  cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0 text-[#ffffff] font-medium">
                   L O G I N
                 </button>
               </form>
